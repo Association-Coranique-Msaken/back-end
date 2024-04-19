fc64b2269c0af1b72b21a0a5fd7f8b7a21f8dc2f
g3ida <med.gaida@gmail.com>
2024-04-19 22:45:09 +0200

    Configure Swagger to find endpoints description

--- src/routes/TeacherRouter.ts
+++ src/routes/TeacherRouter.ts
@@ -5,7 +5,24 @@ import { pagingMiddleware } from "../middlewares/pagingMiddleware";
 
 const teacherRouter = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/teacherapi:
+ *   patch:
+ *     summary: Update teacher.
+ *     description: Requires teacher access token. Updates the teacher.
+ *     tags: [teacherapi]
+ */
 teacherRouter.patch("/teacher", teacherAuthentication, updateData);
+
+/**
+ * @swagger
+ * /api/v1/teacherapi/group/list:
+ *   get:
+ *     summary: Get teacher groups
+ *     description: Requires teacher access token. Returns a list of teacher groups. Supports pagination parameters.
+ *     tags: [teacherapi]
+ */
 teacherRouter.get("/group/list", teacherAuthentication, pagingMiddleware, getGroups);
 
 export default teacherRouter;
